146aeffe9c89ba2a2360ccc17a08589f53f553cd
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-09-21 14:14:17 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -1,1 +1,49 @@
+<?php require_once("header.php"); ?>
+<!doctype html>
+<html lang="en">
 
+  <table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Prefix</th>
+      <th>Number</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+$servername = "localhost";
+$username = "ngoralou_homework3";
+$password = "Createou!";
+$dbname = "ngoralou_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT instructorID, instructorName FROM course";
+$result = $conn->query($sql);
+       
+ if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+
+        echo '<tr>              
+                  <td scope="row">' . $row["instructorID"]. '</td>
+                  <td>' . $row["instructorName"] .'</td>
+                   <br>                
+                </tr>';
+    }
+} else {
+    echo "0 results";
+} 
+?>
+       </tbody>    
+      </table>
+    </div>
+</table>
