d6613dbe1248a187ba3977e99440b56561364835
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-09-28 14:22:24 -0500

    Update header.php

--- header.php
+++ header.php
@@ -25,6 +25,9 @@
           <a class="nav-link" href="section.php">Sections</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link" href="student.php">Students</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="instructor-section.php">Full Table</a>
         </li>
       </ul>
