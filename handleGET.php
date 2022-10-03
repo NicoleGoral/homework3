53d20f930eee59711eeaea373377ef7a875ea709
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-10-02 22:09:20 -0500

    Create handleGET.php

---
+++ handleGET.php
@@ -0,0 +1,8 @@
+<?php require_once("header.php"); ?>
+
+
+<form action="handleGET.php" method="get">
+Name: <input type="text" name="name"><br>
+Year: <input type="text" name="year"><br>
+<input type="submit">
+</form>
