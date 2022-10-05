464183773cb95d865c0a6b29611f30bf6265a392
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-10-04 22:10:34 -0500

    Update postFilter.php

--- postFilter.php
+++ postFilter.php
@@ -22,7 +22,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$cid = $_POST["id"];
+$cid = $_POST["PrefixClass"];
 //echo $iid;
 $sql = "select courseID, prefix, number, description from Courses where courseID=" . $cid;
 //echo $sql;
