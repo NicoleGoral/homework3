d8534f66ff9dbe78fe1ad8401d13daf3b361438a
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-10-04 22:13:35 -0500

    Update submitPost.php

--- submitPost.php
+++ submitPost.php
@@ -1,3 +1,9 @@
 <?php require_once("header.php")?>
 
-haha my name is nicole
+
+<form action="postFilter.php" method="post">
+
+Prefix : <input type="text" name="PrefixClass"><br>
+
+<input type="Post">
+    
