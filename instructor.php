fc0c9abf25ac163501fd55a4d731fb4495fef3c1
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-10-12 13:51:18 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -50,6 +50,7 @@ $result = $conn->query($sql);
     </div>
 </table>
 
+//add instructor
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -70,3 +71,7 @@ $result = $conn->query($sql);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
+
+//edit instructor
+
+//delete instructor
