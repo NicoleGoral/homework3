9f750880de85508b63b7609a212e74d1e77bafe8
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-10-17 13:45:12 -0500

    Create coursefile.php

---
+++ coursefile.php
@@ -0,0 +1,47 @@
+<?php require_once("header.php"); ?>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Employees</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+$servername = "localhost";
+$username = "ngoralou_homework3";
+$password = "Createou!";
+$dbname = "ngoralou_homework3";
+    
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$cid = $_GET["courseID"];
+$sql = "SELECT courseID, coursePrefix, courseNumber, courseDescription FROM course c Where courseID =" . $cid;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    ?>
+<tr>
+  <td><?=$row["courseID"]?></td>
+  <td><?=$row["coursePrefix"]?></td>
+  <td><?=$row["courseNumber"]?></td>
+  <td><?=$row["courseDescription"]?></td>
+</tr>
+  <?php
+    }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+</table>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
