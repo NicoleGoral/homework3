637c3c33a54d3d8ec656fca4907b5a70c2eb0946
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-10-17 13:52:21 -0500

    Update student.php

--- student.php
+++ student.php
@@ -1,8 +1,5 @@
 <?php require_once("header.php"); ?>
-<!doctype html>
-<html lang="en">
-
-  <table class="table table-striped">
+<table class="table table-striped">
   <thead>
     <tr>
       <th>Student ID</th>
@@ -17,21 +14,21 @@ $servername = "localhost";
 $username = "ngoralou_homework3";
 $password = "Createou!";
 $dbname = "ngoralou_homework3";
-
+    
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT studentID, studentName, gradeLevel, courseID FROM student";
 $result = $conn->query($sql);
-       
- if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-?>
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    ?>
 <tr>
   <td><?=$row["studentID"]?></td>
   <td><?=$row["studentName"]?></td>
@@ -39,13 +36,15 @@ $result = $conn->query($sql);
   <td><?=$row["courseID"]?></td>
   <td><a href="coursefile.php?id=<?=$row["courseID"]?>"><?=$row["courseID"]?></a></td>
 </tr>
-       
+  <?php
     }
 } else {
-    echo "0 results";
-} 
+  echo "0 results";
+}
+$conn->close();
 ?>
-       </tbody>    
-      </table>
-    </div>
+  </tbody>
 </table>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
